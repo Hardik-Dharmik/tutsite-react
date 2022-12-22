1067dbf26f4b937cc6d9d6788bfd0fe5ea29f8d6
Hardik <hardikdharmik94@gmail.com>
2022-12-22 19:28:01 +0530

    Header and Intro done

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import "../css/Header.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+function Header() {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/768px-React-icon.svg.png?20220125121207"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+
+            <NavDropdown title="Courses" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                8th to 1oth State and CBSE
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">
+                11th and 12th State
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">JEE</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">NEET</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link">Faculties</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+          <div className="header__leftIcons">
+            <FacebookIcon />
+            <YouTubeIcon />
+            <InstagramIcon />
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,43 @@
+import Button from "@mui/material/Button";
+import { TypeAnimation } from "react-type-animation";
+import "../css/Main.css";
+
+function Main() {
+  return (
+    <div className="jumbotron">
+      <div className="main text-center py-5">
+        <h3>fvngjb</h3>
+        <h1>Education</h1>
+        <TypeAnimation
+          sequence={[
+            "8th and 9th State and CBSE",
+            1000,
+            "10th State and CBSE",
+            1000,
+            "11th and 12th State and CBSE",
+            1000,
+            "JEE",
+            1000,
+            "NEET",
+            1000,
+          ]}
+          wrapper="div"
+          cursor={true}
+          repeat={Infinity}
+          style={{ fontSize: "2em" }}
+        />
+
+        <button className="main__btn">Contact Now</button>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#fff"
+          fill-opacity="1"
+          d="M0,256L48,250.7C96,245,192,235,288,234.7C384,235,480,245,576,250.7C672,256,768,256,864,229.3C960,203,1056,149,1152,138.7C1248,128,1344,160,1392,176L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </div>
+  );
+}
+
+export default Main;
